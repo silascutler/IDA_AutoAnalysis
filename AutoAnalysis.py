8ae8d8b552f759b3380f096ba1d83be30ee3f7a8
Silas Cutler <silas.cutler@blacklistthisdomain.com>
2018-02-20 21:12:06 -0500

    Added UI

--- AutoAnalysis.py
+++ AutoAnalysis.py
@@ -1,7 +1,90 @@
 import idc
 import idaapi
 import idautils
+from idaapi import PluginForm
+from PyQt5 import QtCore, QtGui
+
 from AutoAnalysis import definitions
+
+data = []
+
+class AutoAnalysisOutput(PluginForm):
+	#Based on code found @https://pastebin.com/raw/nMdTre1F
+	def OnCreate(self, form):
+		self.parent = self.FormToPyQtWidget(form)
+		self.PopulateForm()
+
+	def PopulateForm(self):
+		layout = QtWidgets.QVBoxLayout()
+
+		self.table = QtWidgets.QTableWidget()
+		layout.addWidget(self.table)
+
+		self.table.setColumnCount(3 + len(definitions.PEAPIs.keys()))
+
+		self.table.setColumnWidth(0, 100)
+		self.table.setHorizontalHeaderItem(0, QtWidgets.QTableWidgetItem("Address"))
+
+		self.table.setColumnWidth(1, 250)
+		self.table.setHorizontalHeaderItem(1, QtWidgets.QTableWidgetItem("Funciton"))
+
+		self.table.setColumnWidth(2, 2)
+		self.table.setHorizontalHeaderItem(2, QtWidgets.QTableWidgetItem(""))
+
+		for index, name in enumerate(definitions.PEAPIs.keys()):
+
+			self.table.setColumnWidth(3 + index, len(name)*10)
+			self.table.setHorizontalHeaderItem(3 + index, QtWidgets.QTableWidgetItem(name))
+
+		self.table.cellDoubleClicked.connect(self.double_clicked)
+
+		self.table.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+		self.parent.setLayout(layout)
+
+		self.fill_table()
+
+	def fill_table(self):
+		self.table.setRowCount(len(data))
+
+		row = 0
+		for titem in data:
+			
+
+			item = QtWidgets.QTableWidgetItem('0x%X' % titem[0])
+			item.setFlags(item.flags() ^ QtCore.Qt.ItemIsEditable)
+			self.table.setItem(row, 0, item)
+
+			item = QtWidgets.QTableWidgetItem(titem[1])
+			item.setFlags(item.flags() ^ QtCore.Qt.ItemIsEditable)
+			self.table.setItem(row, 1, item)
+
+			item = QtWidgets.QTableWidgetItem(titem[2])
+			self.table.setItem(row, 2, item)
+
+			for col in range(0, len(definitions.PEAPIs.keys()) ):
+				item = QtWidgets.QTableWidgetItem(titem[3 + col])
+				self.table.setItem(row, 2 + col, item)
+
+			row += 1
+
+		self.table.resizeRowsToContents()
+
+	def double_clicked(self, row, column):
+		if column == 2:
+			return
+		
+		idc.Jump(data[row][0])
+
+
+	def OnClose(self, form):
+		"""
+		Called when the plugin form is closed
+		"""
+		pass
+
+	def Show(self):
+		return PluginForm.Show(self, "AutoAnalysis")
+
 
 
 # Init plugin
@@ -22,6 +105,8 @@ class autoAnalysis_t(idaapi.plugin_t):
 
 	def run(self, arg):
 		self.functionParse()
+		ui = AutoAnalysisOutput()
+		ui.Show()
 		pass
 
 
@@ -71,12 +156,20 @@ class autoAnalysis_t(idaapi.plugin_t):
 						n_flags = ( t_flags| n_flags )
 		# Rename function if flags populated
 		# 	Skip of this isn't the first run
+		sflags = "".join(set(n_flags))
 		if len(n_flags) > 0 and self.rename:
 			fn = idc.GetFunctionName(funcea)
-			sflags = "".join(set(n_flags))
 			if not fn.startswith(sflags):
 				print "Renaming - ", fn, " with - ", sflags
 				idc.MakeName(funcea, str(sflags + "_" + fn ))
+		tbl = [ funcea, idc.GetFunctionName(funcea), sflags ]
+		for f in definitions.PEAPIs.keys():
+			if definitions.PEAPIs[f]['flag'] in sflags:
+				tbl.append('*')
+			else:
+				tbl.append('')
+
+		data.append( tbl )
 
 	def processFunction(self, funcea, apiName):
 		t_flags = set()
@@ -91,7 +184,6 @@ class autoAnalysis_t(idaapi.plugin_t):
 					t_flags.add(definitions.PEAPIs[fType]['flag'])
 			return t_flags
 		# This is where the else statement goes for other archs
-
 		print apiName
 		return set()
 
